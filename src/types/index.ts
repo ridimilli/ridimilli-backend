626d9c8ff098ee8c4ce65524baa3e66460dae175
JunyupHong <junyup.hong@gmail.com>
2021-02-07 18:26:33 +0900

    [Fix] Crawler platform 설정 오류 수정

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,41 @@
-type ServiceType_T = 'purchase' | 'subscribe';
+enum ServiceType {
+    PURCHASE = 'purchase',
+    SUBSCRIBE = 'subscribe',
+}
+
+type ServiceType_T = ServiceType.PURCHASE | ServiceType.SUBSCRIBE;
+
+enum Platforms {
+    RIDI = '리디북스',
+    MILLIE = '밀리의서재',
+    YES24 = '예스24',
+    KYOBO = '인터넷 교보문고',
+    KYOBO_BASIC = '인터넷 교보문고 BASIC',
+    KYOBO_UNLIMITED = '인터넷 교보문고 UNLIMITED',
+    ALADIN = '알라딘',
+    INTERPARK = '인터파크 도서',
+    NAVER = '네이버 시리즈',
+}
+
+enum SubscribePrice {
+    RIDI = 1000,
+    MILLIE = 2000,
+    YES24 = 3000,
+    KYOBO_BASIC = 4000,
+    KYOBO_UNLIMITED = 5000,
+    ALADIN = 6000,
+    INTERPARK = 7000,
+    NAVER = 8000,
+}
+
 type Platform_T =
-    | '리디북스'
-    | '밀리의서재'
-    | '예스24'
-    | '인터넷 교보문고'
-    | '알라딘'
-    | '인터파크 도서'
-    | '네이버 시리즈';
+    | Platforms.ALADIN
+    | Platforms.MILLIE
+    | Platforms.INTERPARK
+    | Platforms.KYOBO
+    | Platforms.NAVER
+    | Platforms.RIDI
+    | Platforms.YES24;
 
 type NaverBook_T = {
     title: string;
@@ -19,16 +48,6 @@ type NaverBook_T = {
     pubdate: Date;
 };
 
-enum Platforms {
-    RIDI = '리디북스',
-    MILLI = '밀리의서재',
-    YES24 = '예스24',
-    KYOBO = '인터넷 교보문고',
-    ALADIN = '알라딘',
-    INTERPARK = '인터파크 도서',
-    NAVER = '네이버 시리즈',
-}
-
 /**
  * export Type
  */
@@ -37,4 +56,4 @@ export { ServiceType_T, Platform_T, NaverBook_T };
 /**
  * export Enum
  */
-export { Platforms };
+export { Platforms, ServiceType, SubscribePrice };
