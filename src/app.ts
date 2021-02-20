de16c616b8c147e72d90e238077f1069e79d65b7
최영훈 <c_yh0327@naver.com>
2021-02-20 14:05:52 +0900

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import * as express from 'express';
 import * as logger from 'morgan';
+import * as cors from 'cors';
 import indexRouter from '../src/routes';
 
 const app: express.Application = express();
+app.use(
+    cors({
+        credentials: true,
+    })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
