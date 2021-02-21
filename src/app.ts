cdf08422a33510032377c65ebb895f344deb1c87
최영훈 <c_yh0327@naver.com>
2021-02-21 15:58:20 +0900

    refactoring: maxListners 셋팅
    1. maxListner를 100으로 설정.. infinitiy는 겁이나서 못하겠음

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from 'express';
 import * as express from 'express';
 import * as logger from 'morgan';
 import * as cors from 'cors';
@@ -14,5 +15,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(500);
+    res.json({
+        message: err.message,
+        error: err,
+    });
+});
 
 export default app;
--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,4 +1,5 @@
 import app from '../app';
+import * as debug from 'debug';
 import { createServer } from 'http';
 
 const port: Number = Number(process.env.PORT) || 3000;
@@ -8,5 +9,44 @@ const server = createServer(app);
 server.listen(port, () => {
     console.log(`${port}포트 서버 대기 중!`);
 });
+server.on('error', onError);
+server.on('listening', onListening);
+process.setMaxListeners(100);
 
+/**
+ * Event listner for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    //handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+/**
+ * http 서버 이벤트 리스너
+ */
+
+function onListening() {
+    const addr = server.address();
+    const bind =
+        typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+
+    debug('Listening on ' + bind);
+}
 export default server;
